15f568b20e74e7feca09b5a1d4f2d82d87a7c89e
Ken <smartestrobotdai@gmail.com>
2019-04-02 16:48:44 +0200

    clean files

--- config.js
+++
@@ -1,9 +0,0 @@
-let pgConfig = {
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'dai',
-  port: 5434
-}
-
-exports.pgConfig = pgConfig;
---
+++ data-sink/config.js
@@ -0,0 +1,9 @@
+let pgConfig = {
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'dai',
+  port: 5434
+}
+
+exports.pgConfig = pgConfig;
