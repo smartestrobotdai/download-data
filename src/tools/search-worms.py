611d200aeeed08c2952ea6eb0859c8dc502db1b1
smartestrobotdai <smartestrobotdai@gmail.com>
2019-05-27 12:58:48 +0200

    tools: search-worms supports search worms for stocks other than Nordea

--- src/tools/search-worms.py
+++ src/tools/search-worms.py
@@ -3,15 +3,17 @@ import os.path
 sys.path.append("../")
 from stockwormmanager import StockWormManager
 
-if len(sys.argv) < 3:
-	print("usage: python3 search-worms.py start_day_index end_day_index [is_test]")
+if len(sys.argv) < 5:
+	print("usage: python3 search-worms.py stock_name, stock_index, start_day_index end_day_index [is_test]")
 	sys.exit()
 
-start_day_index = int(sys.argv[1])
-end_day_index = int(sys.argv[2])
+stock_name = sys.argv[1]
+stock_index = int(sys.argv[2])
+start_day_index = int(sys.argv[3])
+end_day_index = int(sys.argv[4])
 is_test = False
-if len(sys.argv) == 4:
-	is_test = bool(sys.argv[3])
+if len(sys.argv) == 6:
+	is_test = bool(sys.argv[5])
 
-stock_worm_manager = StockWormManager('Nordea', 5, '../../models', '../../preprocessed-data')
+stock_worm_manager = StockWormManager(stock_name, stock_index, '../../models', '../../preprocessed-data')
 stock_worm_manager.search_worms(0, 60, is_test=is_test)
