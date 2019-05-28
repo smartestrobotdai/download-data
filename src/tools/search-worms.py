c83fd46f17f1a2315e78c09860f699b982a37d5d
smartestrobotdai <smartestrobotdai@gmail.com>
2019-05-28 22:39:06 +0200

    search-worms: minor changes

--- src/tools/search-worms.py
+++ src/tools/search-worms.py
@@ -17,7 +17,6 @@ search_strategy = False
 if len(sys.argv) >= 6:
 	search_strategy = bool(sys.argv[5])
 
-
 is_test = False
 if len(sys.argv) >= 7:
 	is_test = bool(sys.argv[6])
@@ -26,9 +25,9 @@ stock_data_dir = get_stock_data_dir()
 preprocessed_data_dir = get_preprocessed_data_dir()
 
 stock_worm_manager = StockWormManager(stock_name, stock_index, stock_data_dir, preprocessed_data_dir)
-swarm_path = stock_worm_manager.get_swarm_path(0, 60)
+swarm_path = get_swarm_dir(stock_name, stock_index, start_day_index, end_day_index)
 
 if not os.path.isdir(swarm_path):
 	os.makedirs(swarm_path, exist_ok=True)
 
-stock_worm_manager.search_worms(0, 60, is_test=is_test, search_strategy=search_strategy)
+stock_worm_manager.search_worms(start_day_index, end_day_index, is_test=is_test, search_strategy=search_strategy)
